00baca5efbbd9c19f92f254128a478f4f17a0e6c
linjing <linjingljlj@gmail.com>
2013-12-13 16:53:14 +0800

    add code

--- HuaRongDao/src/test_set/test_set.cpp
+++ HuaRongDao/src/test_set/test_set.cpp
@@ -11,27 +11,27 @@ struct mask {
   }
 };
 
-namespace std {
-  template <>
-  struct less<mask> {
-    bool operator () (const mask &l, const mask &r) {
-      return l.str < r.str;
-    }
-  };
-}
-
-bool operator < (const mask &l, const mask &r) {
-  return l.str < r.str;
-}
-
-struct mask_less : public std::binary_function <mask, mask, bool> {
-  bool operator () (const mask &l, const mask &r) const {
-    return l.str < r.str;
-  }
-};
+//namespace std {
+//  template <>
+//  struct less<mask> {
+//    bool operator () (const mask &l, const mask &r) {
+//      return l.str < r.str;
+//    }
+//  };
+//}
+//
+//bool operator < (const mask &l, const mask &r) {
+//  return l.str < r.str;
+//}
+//
+//struct mask_less : public std::binary_function <mask, mask, bool> {
+//  bool operator () (const mask &l, const mask &r) const {
+//    return l.str < r.str;
+//  }
+//};
 
 int main (int argc, char **argv) {
-  set<mask, mask_less> masks;
+  set<mask> masks;
   mask a; a.str = "hello";
   mask b;
   if (argc == 1)
