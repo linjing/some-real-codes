34e00272069e314db7d51f1510386d8b5c8ab577
linjing <linjingljlj@gmail.com>
2013-12-14 13:46:31 +0800

    use gprof to test

--- HuaRongDao/src/build.sh
+++ HuaRongDao/src/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -g -o  BFS_huarongdao huarongdao.cpp  breadth_first_search.cpp  -I . --std=c++0x
+g++ -pg -g -o  BFS_huarongdao huarongdao.cpp  breadth_first_search.cpp  -I . --std=c++0x
