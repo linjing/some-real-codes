d2917b36a1811860c76ac83b5a8ffe16237e9de3
linjing <linjingljlj@gmail.com>
2013-12-19 10:55:24 +0800

    add init_board function

--- HuaRongDao/src/breadth_first_search.cpp
+++ HuaRongDao/src/breadth_first_search.cpp
@@ -8,38 +8,41 @@
 
 using namespace std;
 
+void init_board (chessboard &input_board) {
+  chess gy (chess_type::guan_yu, point (2, 1));
+  input_board.push_chess (gy);
+  chess zf (chess_type::zhang_fei, point (0, 0));
+  input_board.push_chess (zf);
+  chess zy (chess_type::zhao_yun, point (0, 3));
+  input_board.push_chess (zy);
+  chess mc (chess_type::ma_chao, point (2, 0));
+  input_board.push_chess (mc);
+  chess hz (chess_type::huang_zhong, point (2, 3));
+  input_board.push_chess (hz);
+
+  chess cc (chess_type::cao_cao, point (0, 1));
+  input_board.push_chess (cc);
+
+  chess z1 (chess_type::zu, point (4, 0));
+  input_board.push_chess (z1);
+  chess z2 (chess_type::zu, point (3, 1));
+  input_board.push_chess (z2);
+  chess z3 (chess_type::zu, point (3, 2));
+  input_board.push_chess (z3);
+  chess z4 (chess_type::zu, point (4, 3));
+  input_board.push_chess (z4);
+}
+
 int main () {
   unordered_set<board_mask> seen; // add known board
   deque<chessboard> queue;
 
-  // input init 
-  chessboard init_chesses;
+  // init, fill chess to board
+  chessboard input_board;
+  init_board (input_board);
 
-  chess gy (chess_type::guan_yu, point (2, 1));
-  init_chesses.push_chess (gy);
-  chess zf (chess_type::zhang_fei, point (0, 0));
-  init_chesses.push_chess (zf);
-  chess zy (chess_type::zhao_yun, point (0, 3));
-  init_chesses.push_chess (zy);
-  chess mc (chess_type::ma_chao, point (2, 0));
-  init_chesses.push_chess (mc);
-  chess hz (chess_type::huang_zhong, point (2, 3));
-  init_chesses.push_chess (hz);
-
-  chess cc (chess_type::cao_cao, point (0, 1));
-  init_chesses.push_chess (cc);
-
-  chess z1 (chess_type::zu, point (4, 0));
-  init_chesses.push_chess (z1);
-  chess z2 (chess_type::zu, point (3, 1));
-  init_chesses.push_chess (z2);
-  chess z3 (chess_type::zu, point (3, 2));
-  init_chesses.push_chess (z3);
-  chess z4 (chess_type::zu, point (4, 3));
-  init_chesses.push_chess (z4);
-
-  queue.push_back (init_chesses);
-  board_mask bm (init_chesses);
+  queue.push_back (input_board);
+  board_mask bm (input_board);
   seen.insert (bm);
 
   // BFS to results
