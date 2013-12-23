a4b6fa1975b8865d99b7b42f3ada07f3cfda24ef
linjing <linjingljlj@gmail.com>
2013-12-23 20:22:26 +0800

    remove DUSE_LAMBA

--- HuaRongDao/src/build.sh
+++ HuaRongDao/src/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #flags -pg  -DDEBUG=1 to test
-g++ -O3 -g -o  BFS_huarongdao huarongdao.cpp  breadth_first_search.cpp  -I . --std=c++0x -DUSE_LAMBDA=1
+g++ -O3 -g -o  BFS_huarongdao huarongdao.cpp  breadth_first_search.cpp  -I . --std=c++0x 
