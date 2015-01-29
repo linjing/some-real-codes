69374151f12e1251a22ca609ac93daa38fb7cb21
linjing <linjingljlj@gmail.com>
2015-01-29 16:32:08 +0800

    update

--- time/timestamp.cpp
+++ time/timestamp.cpp
@@ -1,13 +1,33 @@
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
+
+void format_time (); 
+void use_ctime ();
+
 int main ()
 {
-    char str[1024];
-    time_t ts = time (NULL);
-    strftime (str, sizeof(str)-1, "%Y-%m-%dT%H:%M:%S", gmtime (&ts));
-    printf  ("%s\n", str);
-    return 0;
-
+  format_time ();
+  use_ctime ();
+  return 0;
 }
 
+void use_ctime ()
+{
+  time_t ts = time (NULL);
+  printf  ("Readable time: %s\n", ctime (&ts));
+}
 
+void format_time ()
+{
+  char str[1024];
+  time_t ts = time (NULL);
+  /* format time xxxx-xx-xxTxx:xx:xx */
+  strftime (str, sizeof(str)-1, "%Y-%m-%dT%H:%M:%S", gmtime (&ts));
+  printf  ("UTC time(3339): %s\n", str);
+
+  struct tm tm_ts;
+  strptime (str, "%Y-%m-%dT%H:%M:%S", &tm_ts);
+  strftime (str, sizeof(str)-1, "%H:%M:%S %Y-%m-%d", &tm_ts);
+  printf  ("UTC time [strf/ptime]: %s\n", str);
+}
